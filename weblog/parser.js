556cda71c180433a29f8c923632b2dda0d8c5f1c
Nahin Bin Kaysar <nahinbinkaysar@gmail.com>
2024-08-10 09:16:10 +0600

    big update

--- weblog/parser.js
+++ weblog/parser.js
@@ -1,7 +1,25 @@
 // parser.js
 
+// Function to escape HTML entities in code blocks
+function escapeHtml(code) {
+    return code.replace(/&/g, "&amp;")
+               .replace(/</g, "&lt;")
+               .replace(/>/g, "&gt;");
+}
+
 // Function to parse Markdown to HTML
 function parseMarkdown(markdownText) {
+    // Temporarily replace code blocks with placeholders
+    const codeBlocks = [];
+    markdownText = markdownText.replace(/```(.*?)```/gs, (match, code) => {
+        const placeholder = `{{CODE_BLOCK_${codeBlocks.length}}}`;
+        codeBlocks.push(`<pre><code>${escapeHtml(code)}</code></pre>`);
+        return placeholder;
+    });
+
+    // Parse inline code (fenced with single backticks `)
+    markdownText = markdownText.replace(/`(.*?)`/g, '<code>$1</code>');
+
     // Parse headings
     markdownText = markdownText.replace(/^(#{1,6})\s*(.*)/gm, (match, hashes, content) => {
         const level = hashes.length;
@@ -19,6 +37,12 @@ function parseMarkdown(markdownText) {
 
     // Parse links
     markdownText = markdownText.replace(/\[([^\[]+)\]\(([^\)]+)\)/g, '<a href="$2">$1</a>');
+
+    // Restore code blocks
+    codeBlocks.forEach((code, index) => {
+        const placeholder = `{{CODE_BLOCK_${index}}}`;
+        markdownText = markdownText.replace(placeholder, code);
+    });
 
     return markdownText;
 }

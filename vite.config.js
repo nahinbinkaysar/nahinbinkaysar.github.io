b9f8e59806d72b3a85e7797cb58a2c21376ddf0d
Nahin Bin Kaysar <nahinbinkaysar@gmail.com>
2025-12-30 22:33:21 +0600

    react

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/",
-  build: {
-    outDir: "../dist",
-    emptyOutDir: true,
-  },
   plugins: [react()],
 })

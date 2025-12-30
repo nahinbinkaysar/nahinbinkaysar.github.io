b39407724d8e91c3357770b4484a43e43b9782d7
Nahin Bin Kaysar <nahinbinkaysar@gmail.com>
2025-12-30 22:59:25 +0600

    some changes

---
+++ src/components/Home.jsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button"
+
+function Home() {
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button variant="outline">Click me</Button>
+    </div>
+  )
+}
+
+export { Home };
